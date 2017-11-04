30af6678c2931f22ed966100f37861f542339091
rick <sasquatch@rickvenn.com>
2017-11-04 12:22:37 -0700

    move backtick into function layer for easy tab

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -128,7 +128,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, ___,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T,Key_Escape,
+   Key_Tab, Key_Q, Key_W, Key_E, Key_R, Key_T,Key_Escape,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftGui,
    Key_LeftControl, Key_Backspace, Key_LeftAlt, Key_LeftShift,
@@ -143,7 +143,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [FUNCTION] =  KEYMAP_STACKED
   (Key_CapsLock,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
-   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Backtick,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
